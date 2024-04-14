403fb9fe750d5811beee1f19c6220c3cb76d9cfc
0HMyC <okamithegod@gmail.com>
2024-04-14 01:22:48 -0700

    Update PmdTarget_Message.cs
    * Remove Data variable; not needed with changes to PmdTargetType
    * Add SetLocalFlag; used when message called is a SEL prompt, stores the selected value into the local flag with the corresponding ID
    * Add Field16; seemingly unused
    * Use byte for MessageMode, accurate to file
    * Add MessageAccessMode; affects how MessageIndex is used to load message from BMD
    * Add MessageDisplayMode; determines if message window should be displayed as normal or a tutorial prompt
    * Correct Data2 size

--- Libellus Library/Event/Types/Frame/PmdTarget_Message.cs
+++ Libellus Library/Event/Types/Frame/PmdTarget_Message.cs
@@ -6,45 +6,72 @@ namespace LibellusLibrary.Event.Types.Frame
     internal class PmdTarget_Message : PmdTargetType
     {
         [JsonPropertyOrder(-100)]
-        [JsonConverter(typeof(ByteArrayToHexArray))]
-        public byte[] Data { get; set; }
-
-        // [JsonPropertyOrder(-99)]
-        // public PmdFlags Flags { get; set; }
-
+        public byte MessageIndex { get; set; }
+        
         [JsonPropertyOrder(-99)]
-        public uint MessageIndex { get; set; }
+        public byte SetLocalFlag { get; set; }
 
         [JsonPropertyOrder(-98)]
+        public ushort Field16 { get; set; }
+
+        [JsonPropertyOrder(-97)]
         [JsonConverter(typeof(JsonStringEnumConverter))]
         public MessageModeEnum MessageMode { get; set; }
 
-        [JsonPropertyOrder(-97)]
+        [JsonPropertyOrder(-96)]
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public AccessModeEnum MessageAccessMode { get; set; }
+
+        [JsonPropertyOrder(-95)]
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public DisplayModeEnum MessageDisplayMode { get; set; }
+
+        [JsonPropertyOrder(-94)]
         [JsonConverter(typeof(ByteArrayToHexArray))]
         public byte[] Data2 { get; set; }
 
-        internal enum MessageModeEnum : uint
+        internal enum MessageModeEnum : byte
         {
             STOP = 0,
             NO_STOP = 1,
         }
 
+        internal enum AccessModeEnum : byte
+        {
+            DIRECT = 0,
+            REF0 = 1,
+            REF1 = 2,
+            REF2 = 3,
+            REF3 = 4,
+            REF4 = 5,
+            JOUTYUU = 6,
+        }
+
+        internal enum DisplayModeEnum : byte
+        {
+            NORMAL = 0,
+            TUTORIAL = 1,
+        }
+
         protected override void ReadData(BinaryReader reader)
         {
-            Data = reader.ReadBytes(4);
-            //Flags = new PmdFlags();
-            //Flags.ReadData(reader);
-            MessageIndex = reader.ReadUInt32();
-            MessageMode = (MessageModeEnum)reader.ReadUInt32();
-            Data2 = reader.ReadBytes(32);
+            MessageIndex = reader.ReadByte();
+            SetLocalFlag = reader.ReadByte();
+            Field16 = reader.ReadUInt16();
+            MessageMode = (MessageModeEnum)reader.ReadByte();
+            MessageAccessMode = (AccessModeEnum)reader.ReadByte();
+            MessageDisplayMode = (DisplayModeEnum)reader.ReadByte();
+            Data2 = reader.ReadBytes(33);
         }
 
         protected override void WriteData(BinaryWriter writer)
         {
-            writer?.Write(Data);
-            // Flags.WriteData(writer);
-            writer?.Write(MessageIndex);
-            writer?.Write((uint)MessageMode);
+            writer?.Write((byte)MessageIndex);
+            writer?.Write((byte)SetLocalFlag);
+            writer?.Write((ushort)Field16);
+            writer?.Write((byte)MessageMode);
+            writer?.Write((byte)MessageAccessMode);
+            writer?.Write((byte)MessageDisplayMode);
             writer?.Write(Data2);
         }
 
