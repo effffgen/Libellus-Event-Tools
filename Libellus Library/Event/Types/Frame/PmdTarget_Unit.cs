e20856eb02c0d1b1e4dff8e682d2b3ed1d591805
0HMyC <okamithegod@gmail.com>
2024-04-14 13:44:21 -0700

    Update PmdTarget_Unit.cs
    * Move HokanTypeEnum and MoveTypeEnum outside of base PmdTarget_Unit class
    * Attempt to make class that PmdTarget_Unit will attach to itself based on the value of PmdTarget_Unit.UnitType, total failure as I do not know what I am doing

--- Libellus Library/Event/Types/Frame/PmdTarget_Unit.cs
+++ Libellus Library/Event/Types/Frame/PmdTarget_Unit.cs
@@ -28,19 +28,6 @@ namespace LibellusLibrary.Event.Types.Frame
             SCALE_CHANGE = 10,
         }
         
-        internal enum HokanTypeEnum : byte
-        {
-            POS = 0,
-            BEZIER = 1,
-            DIRECT = 2,
-        }
-        
-        internal enum MoveTypeEnum : byte
-        {
-            SPEED = 0,
-            LENGTH = 1,
-        }
-        
         protected override void ReadData(BinaryReader reader)
         {
             UnitType = (UnitTypeEnum)reader.ReadUInt32();
@@ -53,4 +40,66 @@ namespace LibellusLibrary.Event.Types.Frame
             writer?.Write(Data);
         }
     }
+    
+    /*public class Unit_SetPositionDirect
+    {
+        [JsonPropertyOrder(-90)]
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public HokanTypeEnum HOKANTYPE { get; set; }
+        
+        [JsonPropertyOrder(-89)]
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public MoveTypeEnum MOVETYPE { get; set; }
+        
+        [JsonPropertyOrder(-88)]
+        public short SPEED { get; set; }
+        
+        [JsonPropertyOrder(-87)]
+        public float PosX { get; set; }
+        
+        [JsonPropertyOrder(-86)]
+        public float PosY { get; set; }
+        
+        [JsonPropertyOrder(-85)]
+        public float PosZ { get; set; }
+        
+        [JsonPropertyOrder(-84)]
+        public byte LOOP { get; set; }
+        
+        [JsonPropertyOrder(-83)]
+        public byte MUSI_STOP { get; set; }
+        
+        [JsonPropertyOrder(-82)]
+        public byte AUTO_ROT { get; set; }
+        
+        [JsonPropertyOrder(-81)]
+        [JsonConverter(typeof(ByteArrayToHexArray))]
+        public byte[] Data { get; set; }
+        
+        [JsonPropertyOrder(-80)]
+        public byte PlaySound { get; set; }
+        
+        [JsonPropertyOrder(-79)]
+        public byte ChannelNumber { get; set; 
+        
+        [JsonPropertyOrder(-78)]
+        public byte SoundInterval { get; set; }
+        
+        [JsonPropertyOrder(-77)]
+        public byte SoundWait { get; set; }
+        
+        internal enum HokanTypeEnum : byte
+        {
+            POS = 0,
+            BEZIER = 1,
+            DIRECT = 2,
+        }
+        
+        internal enum MoveTypeEnum : byte
+        {
+            SPEED = 0,
+            LENGTH = 1,
+        }
+    }*/
+    
 }
