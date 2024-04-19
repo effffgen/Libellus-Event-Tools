82e1a7895eac66d9291c3c8fa3e68432c0b89d54
0HMyC <okamithegod@gmail.com>
2024-04-18 17:50:14 -0700

    Implement POS_CHANGE_D in PmdTarget_Unit
    * Uses json polymorphism to essentially "extend" the PmdTarget_Unit class with the SetPositionDirect class; this is necessary as the data is stored and read differently based on the "UnitType"

--- Libellus Library/Event/Types/Frame/PmdTarget_Unit.cs
+++ Libellus Library/Event/Types/Frame/PmdTarget_Unit.cs
@@ -10,8 +10,7 @@ namespace LibellusLibrary.Event.Types.Frame
         public UnitTypeEnum UnitType { get; set; }
 
         [JsonPropertyOrder(-91)]
-        [JsonConverter(typeof(ByteArrayToHexArray))]
-        public byte[] Data { get; set; }
+        public Unit UnitData { get; set; }
 
         internal enum UnitTypeEnum : uint
         {
@@ -27,79 +26,144 @@ namespace LibellusLibrary.Event.Types.Frame
             SHADOW_ONOFF = 9,
             SCALE_CHANGE = 10,
         }
-        
+
         protected override void ReadData(BinaryReader reader)
         {
             UnitType = (UnitTypeEnum)reader.ReadUInt32();
-            Data = reader.ReadBytes(36);
+            UnitData = UnitType switch
+            {
+                UnitTypeEnum.POS_CHANGE_D => new SetPositionDirect(),
+                _ => new UnknownUnit()
+            };
+            UnitData.ReadData(reader);
         }
 
         protected override void WriteData(BinaryWriter writer)
         {
             writer?.Write((uint)UnitType);
+            UnitData.WriteData(writer);
+        }
+    }
+
+    [JsonDerivedType(typeof(UnknownUnit))]
+    [JsonDerivedType(typeof(SetPositionDirect))]
+    public class Unit
+    {
+        public virtual void ReadData(BinaryReader reader) => throw new InvalidOperationException();
+        public virtual void WriteData(BinaryWriter writer) => throw new InvalidOperationException();
+    }
+
+    public class UnknownUnit : Unit
+    {
+        [JsonPropertyOrder(-90)]
+        [JsonConverter(typeof(ByteArrayToHexArray))]
+        public byte[] Data { get; set; }
+
+        public override void ReadData(BinaryReader reader)
+        {
+            Data = reader.ReadBytes(36);
+        }
+
+        public override void WriteData(BinaryWriter writer)
+        {
             writer?.Write(Data);
         }
     }
-    
-    /*public class Unit_SetPositionDirect
+
+    public class SetPositionDirect : Unit
     {
         [JsonPropertyOrder(-90)]
         [JsonConverter(typeof(JsonStringEnumConverter))]
         public HokanTypeEnum HOKANTYPE { get; set; }
-        
+
         [JsonPropertyOrder(-89)]
         [JsonConverter(typeof(JsonStringEnumConverter))]
         public MoveTypeEnum MOVETYPE { get; set; }
-        
+
         [JsonPropertyOrder(-88)]
         public short SPEED { get; set; }
-        
+
         [JsonPropertyOrder(-87)]
         public float PosX { get; set; }
-        
+
         [JsonPropertyOrder(-86)]
         public float PosY { get; set; }
-        
+
         [JsonPropertyOrder(-85)]
         public float PosZ { get; set; }
-        
+
         [JsonPropertyOrder(-84)]
         public byte LOOP { get; set; }
-        
+
         [JsonPropertyOrder(-83)]
         public byte MUSI_STOP { get; set; }
-        
+
         [JsonPropertyOrder(-82)]
         public byte AUTO_ROT { get; set; }
-        
+
         [JsonPropertyOrder(-81)]
         [JsonConverter(typeof(ByteArrayToHexArray))]
         public byte[] Data { get; set; }
-        
+
         [JsonPropertyOrder(-80)]
         public byte PlaySound { get; set; }
-        
+
         [JsonPropertyOrder(-79)]
-        public byte ChannelNumber { get; set; 
-        
+        public byte ChannelNumber { get; set; }
+
         [JsonPropertyOrder(-78)]
         public byte SoundInterval { get; set; }
-        
+
         [JsonPropertyOrder(-77)]
         public byte SoundWait { get; set; }
-        
-        internal enum HokanTypeEnum : byte
+
+        public enum HokanTypeEnum : byte
         {
             POS = 0,
             BEZIER = 1,
             DIRECT = 2,
         }
-        
-        internal enum MoveTypeEnum : byte
+
+        public enum MoveTypeEnum : byte
         {
             SPEED = 0,
             LENGTH = 1,
         }
-    }*/
-    
+
+        public override void ReadData(BinaryReader reader)
+        {
+            HOKANTYPE = (HokanTypeEnum)reader.ReadByte();
+            MOVETYPE = (MoveTypeEnum)reader.ReadByte();
+            SPEED = reader.ReadInt16();
+            PosX = reader.ReadSingle();
+            PosY = reader.ReadSingle();
+            PosZ = reader.ReadSingle();
+            LOOP = reader.ReadByte();
+            MUSI_STOP = reader.ReadByte();
+            AUTO_ROT = reader.ReadByte();
+            Data = reader.ReadBytes(13);
+            PlaySound = reader.ReadByte();
+            ChannelNumber = reader.ReadByte();
+            SoundInterval = reader.ReadByte();
+            SoundWait = reader.ReadByte();
+        }
+
+        public override void WriteData(BinaryWriter writer)
+        {
+            writer?.Write((byte)HOKANTYPE);
+            writer?.Write((byte)MOVETYPE);
+            writer?.Write(SPEED);
+            writer?.Write(PosX);
+            writer?.Write(PosY);
+            writer?.Write(PosZ);
+            writer?.Write(LOOP);
+            writer?.Write(MUSI_STOP);
+            writer?.Write(AUTO_ROT);
+            writer?.Write(Data);
+            writer?.Write(PlaySound);
+            writer?.Write(ChannelNumber);
+            writer?.Write(SoundInterval);
+            writer?.Write(SoundWait);
+        }
+    }
 }
