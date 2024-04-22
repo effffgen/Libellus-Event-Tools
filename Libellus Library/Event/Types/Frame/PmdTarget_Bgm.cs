f9738ed1734938b95e95b7f1a9b39031a2f7a2f4
0HMyC <okamithegod@gmail.com>
2024-04-21 18:39:55 -0700

    Use tabs in PmdTarget_Bgm.cs

--- Libellus Library/Event/Types/Frame/PmdTarget_Bgm.cs
+++ Libellus Library/Event/Types/Frame/PmdTarget_Bgm.cs
@@ -3,44 +3,44 @@ using System.Text.Json.Serialization;
 
 namespace LibellusLibrary.Event.Types.Frame
 {
-    internal class PmdTarget_Bgm : PmdTargetType
-    {
-        [JsonPropertyOrder(-92)]
-        [JsonConverter(typeof(JsonStringEnumConverter))]
-        public MusicFadeEnum MusicFade { get; set; }
-        
-        [JsonPropertyOrder(-91)]
-        public ushort MusicIndex { get; set; }
-        
-        [JsonPropertyOrder(-90)]
-        [JsonConverter(typeof(ByteArrayToHexArray))]
-        public byte[] Data { get; set; }
-        
-        // This is only accurate for P3; P4(G) removes some of these from the Event Editor menu
-        // TODO: Figure out a way to address that discrepancy
-        internal enum MusicFadeEnum : byte
-        {
-            PLAY = 0,
-            FADEIN = 1,
-            FADEOUT = 2,
-            TRANS = 3,
-            VOLDOWN = 4,
-            VOLUP = 5,
-            ALLSTOP = 6,
-        }
-        
-        protected override void ReadData(BinaryReader reader)
-        {
-            MusicFade = (MusicFadeEnum)reader.ReadUInt16();
-            MusicIndex = reader.ReadUInt16();
-            Data = reader.ReadBytes(36);
-        }
+	internal class PmdTarget_Bgm : PmdTargetType
+	{
+		[JsonPropertyOrder(-92)]
+		[JsonConverter(typeof(JsonStringEnumConverter))]
+		public MusicFadeEnum MusicFade { get; set; }
 
-        protected override void WriteData(BinaryWriter writer)
-        {
-            writer?.Write((ushort)MusicFade);
-            writer?.Write((ushort)MusicIndex);
-            writer?.Write(Data);
-        }
-    }
+		[JsonPropertyOrder(-91)]
+		public ushort MusicIndex { get; set; }
+
+		[JsonPropertyOrder(-90)]
+		[JsonConverter(typeof(ByteArrayToHexArray))]
+		public byte[] Data { get; set; }
+
+		// This is only accurate for P3; P4(G) removes some of these from the Event Editor menu
+		// TODO: Figure out a way to address that discrepancy
+		internal enum MusicFadeEnum : byte
+		{
+			PLAY = 0,
+			FADEIN = 1,
+			FADEOUT = 2,
+			TRANS = 3,
+			VOLDOWN = 4,
+			VOLUP = 5,
+			ALLSTOP = 6,
+		}
+
+		protected override void ReadData(BinaryReader reader)
+		{
+			MusicFade = (MusicFadeEnum)reader.ReadUInt16();
+			MusicIndex = reader.ReadUInt16();
+			Data = reader.ReadBytes(36);
+		}
+
+		protected override void WriteData(BinaryWriter writer)
+		{
+			writer?.Write((ushort)MusicFade);
+			writer?.Write((ushort)MusicIndex);
+			writer?.Write(Data);
+		}
+	}
 }
