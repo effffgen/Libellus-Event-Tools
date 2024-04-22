ac20aeb09c9241d71cff2cd1ad5c04f6b374182a
0HMyC <okamithegod@gmail.com>
2024-04-21 18:38:57 -0700

    Partially revert format changes
    * Use tabs over spaces again; while spaces look much nicer in diffs due to the tab size on github being excessively massive, converting tabs to spaces results in inconsistencies and for a project of this scale, ends up making the diffs much harder to parse in the long run [Program.cs, PmdFrameFactory.cs]

--- Libellus Event Tool/Program.cs
+++ Libellus Event Tool/Program.cs
@@ -3,49 +3,49 @@
 
 namespace LibellusEventTool
 {
-    class Program
-    {
-        static async Task Main(string[] args)
-        {
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
 
-            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
-            System.Diagnostics.FileVersionInfo fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
-            string version = fvi.FileVersion ?? "Null Version"; // Address CS8600 warning
-            Console.WriteLine("Welcome to LEET!\nLibellus Event Editing Tool: v" + version + " \nNow with better syntax!\n");
+			System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+			System.Diagnostics.FileVersionInfo fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
+			string version = fvi.FileVersion ?? "Null Version"; // Address CS8600 warning
+			Console.WriteLine("Welcome to LEET!\nLibellus Event Editing Tool: v" + version + " \nNow with better syntax!\n");
 
-            if (args.Length < 1)
-            {
-                Console.WriteLine("Not Enough args!");
-                Console.WriteLine("Press Any Button To Exit.");
-                Console.ReadKey();
-                return;
-            }
-            foreach (string file in args)
-            {
-                string ext = Path.GetExtension(file).ToLower();
-                if (ext == ".pm1" || ext == ".pm2" || ext == ".pm3")
-                {
-                    Console.WriteLine("Coverting to Json: "+ file);;
-                    PmdReader reader = new PmdReader();
-                    PolyMovieData pmd = await reader.ReadPmd(file);
-                    // the "!" in Path.GetDirectoryName(file)! indicates null forgiveness;
-                    // basically address CS8604 and should be save considering that
-                    // if this code path is running, `file` PROBABLY isn't null anyways
-                    string folder = Path.Combine(Path.GetDirectoryName(file)!, Path.GetFileNameWithoutExtension(file));
-                    await pmd.ExtractPmd(folder, Path.GetFileName(file));
-                    continue;
-                }
+			if (args.Length < 1)
+			{
+				Console.WriteLine("Not Enough args!");
+				Console.WriteLine("Press Any Button To Exit.");
+				Console.ReadKey();
+				return;
+			}
+			foreach (string file in args)
+			{
+				string ext = Path.GetExtension(file).ToLower();
+				if (ext == ".pm1" || ext == ".pm2" || ext == ".pm3")
+				{
+					Console.WriteLine("Coverting to Json: "+ file);;
+					PmdReader reader = new PmdReader();
+					PolyMovieData pmd = await reader.ReadPmd(file);
+					// the "!" in Path.GetDirectoryName(file)! indicates null forgiveness;
+					// basically address CS8604 and should be save considering that
+					// if this code path is running, `file` PROBABLY isn't null anyways
+					string folder = Path.Combine(Path.GetDirectoryName(file)!, Path.GetFileNameWithoutExtension(file));
+					await pmd.ExtractPmd(folder, Path.GetFileName(file));
+					continue;
+				}
 
-                if (ext == ".json")
-                {
-                    Console.WriteLine("Coverting to PMD: "+ file);
-                    PolyMovieData pmd = await PolyMovieData.LoadPmd(Path.Combine(file));
-                    string pmdext = "PM" + pmd.MagicCode[3];
-                    pmd.SavePmd(file + "." + pmdext);
-                }
-            }
-            Console.WriteLine("Press Any Button To Exit.");
-            Console.ReadKey();
-        }
-    }
+				if (ext == ".json")
+				{
+					Console.WriteLine("Coverting to PMD: "+ file);
+					PolyMovieData pmd = await PolyMovieData.LoadPmd(Path.Combine(file));
+					string pmdext = "PM" + pmd.MagicCode[3];
+					pmd.SavePmd(file + "." + pmdext);
+				}
+			}
+			Console.WriteLine("Press Any Button To Exit.");
+			Console.ReadKey();
+		}
+	}
 }
