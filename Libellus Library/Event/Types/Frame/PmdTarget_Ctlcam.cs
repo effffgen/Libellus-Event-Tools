46ea8c978c7d3e776ddb1ce1e2c233d77a31e626
0HMyC <okamithegod@gmail.com>
2024-04-22 17:01:43 -0700

    Add more info to PmdTarget_Ctlcam
    * Identify Field18; is used when CameraMode is ACTIVE, and stores the ID of the unit to "follow", and the "view length" (which ranges from 200 to 3000 in the editor)

--- Libellus Library/Event/Types/Frame/PmdTarget_Ctlcam.cs
+++ Libellus Library/Event/Types/Frame/PmdTarget_Ctlcam.cs
@@ -13,27 +13,36 @@ namespace LibellusLibrary.Event.Types.Frame
         public CameraModeEnum CameraMode { get; set; }
 
         [JsonPropertyOrder(-90)]
-        public uint Field18 { get; set; }
+        public ushort ActiveSelUnit { get; set; }
         
         [JsonPropertyOrder(-89)]
-        public float CameraX { get; set; }
+        public short ActiveViewLength { get; set; } // limited to 200 - 3000 by editor
+
+        // [JsonPropertyOrder(-90)]
+        // public uint Field18 { get; set; }
         
         [JsonPropertyOrder(-88)]
-        public float CameraY { get; set; }
+        public float CameraX { get; set; }
         
         [JsonPropertyOrder(-87)]
-        public float CameraZ { get; set; }
+        public float CameraY { get; set; }
         
         [JsonPropertyOrder(-86)]
-        public float CameraPitch { get; set; }
+        public float CameraZ { get; set; }
         
         [JsonPropertyOrder(-85)]
-        public float CameraYaw { get; set; }
+        public float CameraPitch { get; set; }
         
         [JsonPropertyOrder(-84)]
-        public float CameraFOV { get; set; }
-
+        public float CameraYaw { get; set; }
+        
         [JsonPropertyOrder(-83)]
+        public float CameraFOV { get; set; }
+        
+        [JsonPropertyOrder(-82)]
+        public sbyte CurveID { get; set; }
+
+        [JsonPropertyOrder(-81)]
         [JsonConverter(typeof(ByteArrayToHexArray))]
         public byte[] Data { get; set; }
 
@@ -47,27 +56,31 @@ namespace LibellusLibrary.Event.Types.Frame
         {
             Field14 = reader.ReadUInt16();
             CameraMode = (CameraModeEnum)reader.ReadUInt16();
-            Field18 = reader.ReadUInt32();
+            ActiveSelUnit = reader.ReadUInt16();
+            ActiveViewLength = reader.ReadInt16();
             CameraX = reader.ReadSingle();
             CameraY = reader.ReadSingle();
             CameraZ = reader.ReadSingle();
             CameraPitch = reader.ReadSingle();
             CameraYaw = reader.ReadSingle();
             CameraFOV = reader.ReadSingle();
-            Data = reader.ReadBytes(8);
+            CurveID = reader.ReadSByte();
+            Data = reader.ReadBytes(7);
         }
 
         protected override void WriteData(BinaryWriter writer)
         {
-            writer?.Write((ushort)Field14);
+            writer?.Write(Field14);
             writer?.Write((ushort)CameraMode);
-            writer?.Write((uint)Field18);
-            writer?.Write((float)CameraX);
-            writer?.Write((float)CameraY);
-            writer?.Write((float)CameraZ);
-            writer?.Write((float)CameraPitch);
-            writer?.Write((float)CameraYaw);
-            writer?.Write((float)CameraFOV);
+            writer?.Write(ActiveSelUnit);
+            writer?.Write(ActiveViewLength);
+            writer?.Write(CameraX);
+            writer?.Write(CameraY);
+            writer?.Write(CameraZ);
+            writer?.Write(CameraPitch);
+            writer?.Write(CameraYaw);
+            writer?.Write(CameraFOV);
+            writer?.Write(CurveID);
             writer?.Write(Data);
         }
     }
