2b8b2f58aa5806f3e8664f54fbbe9cb1ca5e4793
0HMyC <okamithegod@gmail.com>
2024-06-07 12:31:51 -0700

    Update PmdTarget_Script.cs
    * Rename Data2 and Data3 to Data and Data2 respectively

--- Libellus Library/Event/Types/Frame/PmdTarget_Script.cs
+++ Libellus Library/Event/Types/Frame/PmdTarget_Script.cs
@@ -13,27 +13,27 @@ namespace LibellusLibrary.Event.Types.Frame
     {
         [JsonPropertyOrder(-92)]
         [JsonConverter(typeof(ByteArrayToHexArray))]
-        public byte[] Data2 { get; set; }
+        public byte[] Data { get; set; }
 
         [JsonPropertyOrder(-91)]
         public ushort ProcedureIndex { get; set; }
 
         [JsonPropertyOrder(-90)]
         [JsonConverter(typeof(ByteArrayToHexArray))]
-        public byte[] Data3 { get; set; } = Array.Empty<byte>();
+        public byte[] Data2 { get; set; } = Array.Empty<byte>();
 
         protected override void ReadData(BinaryReader reader)
         {
-            Data2 = reader.ReadBytes(6);
+            Data = reader.ReadBytes(6);
             ProcedureIndex = reader.ReadUInt16();
-            Data3 = reader.ReadBytes(32);
+            Data2 = reader.ReadBytes(32);
         }
 
         protected override void WriteData(BinaryWriter writer)
         {
-            writer?.Write(Data2);
+            writer?.Write(Data);
             writer?.Write(ProcedureIndex);
-            writer?.Write(Data3);
+            writer?.Write(Data2);
         }
     }
 }
