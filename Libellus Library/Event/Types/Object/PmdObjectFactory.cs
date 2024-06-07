53996e569a38f949b9dc64416b6236ed944cd5d8
0HMyC <okamithegod@gmail.com>
2024-06-07 12:26:39 -0700

    Remove commented-out code in PmdObjectFactory

--- Libellus Library/Event/Types/Object/PmdObjectFactory.cs
+++ Libellus Library/Event/Types/Object/PmdObjectFactory.cs
@@ -10,36 +10,16 @@ namespace LibellusLibrary.Event.Types.Object
 	{
 		public List<PmdObject> ReadObjects(BinaryReader reader, uint typeTableCount)
 		{
-			// var typeIDs = ReadTypes(reader, typeTableCount);
 			List<PmdObject> objects = new();
+
 			for (int i = 0; i < typeTableCount; i++)
 			{
 				PmdObject currentObject = new PmdObject();
 				currentObject.ReadObject(reader);
 				objects.Add(currentObject);
 			}
-			/*foreach (var type in typeIDs)
-			{
-				PmdObject currentObject = new PmdObject();
-				currentObject.ReadObject(reader);
-				objects.Add(currentObject);
-			}*/
 
 			return objects;
 		}
-
-		/*private List<PmdObjectID> ReadTypes(BinaryReader reader, uint typeTableCount)
-		{
-			long originalpos = reader.BaseStream.Position;
-			List<PmdObjectID> types = new List<PmdObjectID>();
-
-			for (int i = 0; i < typeTableCount; i++)
-			{
-				types.Add((PmdObjectID)reader.ReadUInt16());
-				reader.BaseStream.Position += 0x20;
-			}
-			reader.BaseStream.Position = originalpos;
-			return types;
-		}*/
 	}
 }
