822ba0729ffd8fd0e2b5b03dd9ced4a605625faf
0HMyC <okamithegod@gmail.com>
2024-06-20 11:57:54 -0700

    Fix incorrect PmdTarget_Fog extraction
    * Accidentally removed Field14 as I forgot that it was a thing that affected the output of JSON's I was using as refrence, which made the extracted values wrong (oops)
    * Fix JsonPropertyOrder to ensure properties are serialized in the correct order
    * Fix Data length not being adjusted after re-adding Field14

--- Libellus Library/Event/Types/Frame/PmdTarget_Fog.cs
+++ Libellus Library/Event/Types/Frame/PmdTarget_Fog.cs
@@ -6,23 +6,25 @@ namespace LibellusLibrary.Event.Types.Frame
 	internal class PmdTarget_Fog : PmdTargetType
 	{
 		[JsonPropertyOrder(-92)]
-		public FogColours? SkyboxColour { get; set; } // Controls colour of Skybox fog
-
-		[JsonPropertyOrder(-92)]
-		public FogColours? FogColour { get; set; } // Controls colour of fog (affects 3D objects)
-
-		[JsonPropertyOrder(-92)]
-		public float FogNear { get; set; } // Controls how close to the camera the fog begins
-
-		[JsonPropertyOrder(-92)]
-		public float FogFar { get; set; } // Controls how far away until the fog ends
+		public uint Field14 { get; set; }
 
 		[JsonPropertyOrder(-91)]
+		public FogColours? SkyboxColour { get; set; } // Controls colour of Skybox fog
+		[JsonPropertyOrder(-90)]
+		public FogColours? FogColour { get; set; } // Controls colour of fog (affects 3D objects)
+
+		[JsonPropertyOrder(-89)]
+		public float FogNear { get; set; } // Controls how close to the camera the fog begins
+		[JsonPropertyOrder(-88)]
+		public float FogFar { get; set; } // Controls how far away until the fog ends
+
+		[JsonPropertyOrder(-87)]
 		[JsonConverter(typeof(ByteArrayToHexArray))]
 		public byte[] Data { get; set; } = Array.Empty<byte>();
 
 		protected override void ReadData(BinaryReader reader)
 		{
+			Field14 = reader.ReadUInt32();
 			SkyboxColour ??= new();
 			SkyboxColour.Red = reader.ReadByte();
 			SkyboxColour.Green = reader.ReadByte();
@@ -35,11 +37,12 @@ namespace LibellusLibrary.Event.Types.Frame
 			FogColour.Alpha = reader.ReadByte();
 			FogNear = reader.ReadSingle();
 			FogFar = reader.ReadSingle();
-			Data = reader.ReadBytes(24);
+			Data = reader.ReadBytes(20);
 		}
 
 		protected override void WriteData(BinaryWriter writer)
 		{
+			writer.Write(Field14);
 			writer.Write(SkyboxColour!.Red);
 			writer.Write(SkyboxColour!.Green);
 			writer.Write(SkyboxColour!.Blue);
