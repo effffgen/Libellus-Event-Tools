c83510e5e0893721d146315dd6ad1e744a366a1b
0HMyC <okamithegod@gmail.com>
2024-06-20 11:52:11 -0700

    Add more Fog frame info
    * Implements Skybox and Fog colour parameters as well as Near and Far distance parameters

--- Libellus Library/Event/Types/Frame/PmdTarget_Fog.cs
+++ Libellus Library/Event/Types/Frame/PmdTarget_Fog.cs
@@ -6,7 +6,16 @@ namespace LibellusLibrary.Event.Types.Frame
 	internal class PmdTarget_Fog : PmdTargetType
 	{
 		[JsonPropertyOrder(-92)]
-		public uint Field14 { get; set; }
+		public FogColours? SkyboxColour { get; set; } // Controls colour of Skybox fog
+
+		[JsonPropertyOrder(-92)]
+		public FogColours? FogColour { get; set; } // Controls colour of fog (affects 3D objects)
+
+		[JsonPropertyOrder(-92)]
+		public float FogNear { get; set; } // Controls how close to the camera the fog begins
+
+		[JsonPropertyOrder(-92)]
+		public float FogFar { get; set; } // Controls how far away until the fog ends
 
 		[JsonPropertyOrder(-91)]
 		[JsonConverter(typeof(ByteArrayToHexArray))]
@@ -14,14 +23,42 @@ namespace LibellusLibrary.Event.Types.Frame
 
 		protected override void ReadData(BinaryReader reader)
 		{
-			Field14 = reader.ReadUInt32();
-			Data = reader.ReadBytes(36);
+			SkyboxColour ??= new();
+			SkyboxColour.Red = reader.ReadByte();
+			SkyboxColour.Green = reader.ReadByte();
+			SkyboxColour.Blue = reader.ReadByte();
+			SkyboxColour.Alpha = reader.ReadByte(); // Possibly unused/nonfunctional?
+			FogColour ??= new();
+			FogColour.Red = reader.ReadByte();
+			FogColour.Green = reader.ReadByte();
+			FogColour.Blue = reader.ReadByte();
+			FogColour.Alpha = reader.ReadByte();
+			FogNear = reader.ReadSingle();
+			FogFar = reader.ReadSingle();
+			Data = reader.ReadBytes(24);
 		}
 
 		protected override void WriteData(BinaryWriter writer)
 		{
-			writer.Write(Field14);
+			writer.Write(SkyboxColour!.Red);
+			writer.Write(SkyboxColour!.Green);
+			writer.Write(SkyboxColour!.Blue);
+			writer.Write(SkyboxColour!.Alpha);
+			writer.Write(FogColour!.Red);
+			writer.Write(FogColour!.Green);
+			writer.Write(FogColour!.Blue);
+			writer.Write(FogColour!.Alpha);
+			writer.Write(FogNear);
+			writer.Write(FogFar);
 			writer.Write(Data);
 		}
+	}
+
+	internal class FogColours
+	{
+		public byte Red { get; set; }
+		public byte Green { get; set; }
+		public byte Blue { get; set; }
+		public byte Alpha { get; set; }
 	}
 }
