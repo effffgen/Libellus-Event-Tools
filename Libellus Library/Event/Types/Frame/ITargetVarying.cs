073569586cd45b13de0ed3529a93c3ebd506342b
0HMyC <okamithegod@gmail.com>
2024-09-19 20:51:18 -0700

    Add function summary comments in ITargetVarying.cs

--- Libellus Library/Event/Types/Frame/ITargetVarying.cs
+++ Libellus Library/Event/Types/Frame/ITargetVarying.cs
@@ -2,7 +2,16 @@
 {
 	internal interface ITargetVarying
 	{
+		/// <summary>
+		/// Gets the appropriate variant of a given frame function by reading binary data.
+		/// </summary>
+		/// <param name="reader"></param>
+		/// <returns></returns>
 		public PmdTargetType GetVariant(BinaryReader reader);
+		/// <summary>
+		/// Gets the appropriate variant of a given frame function by instance data.
+		/// </summary>
+		/// <returns></returns>
 		public PmdTargetType GetVariant();
 	}
 }
